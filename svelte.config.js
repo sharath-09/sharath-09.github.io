94f7c9e16d78a798cf350b87a6a23bf68fe7393f
Sharath <a1768686@student.adelaide.edu.au>
2025-10-26 17:24:47 +1030

    fix: set environment path variable in config

--- svelte.config.js
+++ svelte.config.js
@@ -42,7 +42,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'process.env.BASE_PATH'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
