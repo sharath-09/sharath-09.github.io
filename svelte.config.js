c5f1c4cab18f09f169e2fc6c198825790f472bc7
Sharath <a1768686@student.adelaide.edu.au>
2025-10-26 17:21:38 +1030

    fix: fixing deployment

--- svelte.config.js
+++ svelte.config.js
@@ -42,7 +42,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/portfolio-website'
+			base: process.argv.includes('dev') ? '' : 'process.env.BASE_PATH'
 		}
 	}
 };
